7c0e3ccfdcafd470c78319119db803ed5c5460b6
leey00nsu <dbstndla1212@naver.com>
2026-06-02 12:45:05 +0900

    fix: load default widget styles in demo

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import 'lee-chat-sdk/style.css'
 import { SupportChatDemo } from './ui/support-chat-demo'
 
 const rootElement = document.querySelector('#root')
